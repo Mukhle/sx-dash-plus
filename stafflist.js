02587853c1342a30f669b41182f7c09dfbb6c141
Mukhle <raze0365@gmail.com>
2020-12-02 04:11:08 +0100

    Change lookup blacklist Change lookup button

--- stafflist.js
+++ stafflist.js
@@ -1,3 +1,5 @@
+const blacklistedLookup = ["STEAM_0:1:48016748","STEAM_0:0:56939043"]
+
 if(user_is_staff) {
 	for(let elt of document.querySelectorAll("div.users-preview__cont")) {
 		let steamid = elt.querySelector(".users-preview__status");
@@ -5,17 +7,14 @@ if(user_is_staff) {
 		if(steamid) {
 			let match = steamid.textContent.match(/STEAM_0:\d:\d+/);
 
-			if(match) {
-				let button = elt.querySelector(".btn-danger");
+			if(match && !blacklistedLookup.includes(match[0])) {
+				let cont = elt.querySelector(".users-preview__props");
 
-				if(match[0] == "STEAM_0:1:48016748") {
-					button.classList.add("disabled");
-					button.innerText = `NoLookup`;
-				}else {
-					button.href = `https://stavox.dk/dash/lookup?lookupid=${match[0]}`;
-					button.target = "_blank";
-					button.innerText = `Lookup`;
-				}
+				let button = document.createElement('div');
+				button.className = "users-preview__prop";
+				button.innerHTML = `<a href="https://stavox.dk/dash/lookup?lookupid=${match[0]}" target="_blank"><i class="fa fa-search"></i><span class="users-preview__status" title="Lookup"><b>Lookup</b></span></a>`
+
+				cont.appendChild(button);
 			}
 		}
 	}
