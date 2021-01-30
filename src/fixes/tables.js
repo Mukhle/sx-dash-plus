0e7cd78b33bf297a9239f2ed7d4d74561f690970
Michael Hansen <raze0365@gmail.com>
2021-01-31 00:27:50 +0100

    Give up on changing table cursor

--- src/fixes/tables.js
+++ src/fixes/tables.js
@@ -1,25 +1,8 @@
 const unbind_event_listeners = (node) => {
-	var parent = node.parentNode;
-	if (parent) {
-		parent.replaceChild(node.cloneNode(true), node);
-	} else {
-		var ex = new Error(
-			"Cannot remove event listeners from detached or document nodes"
-		);
-		ex.code = DOMException[(ex.name = "HIERARCHY_REQUEST_ERR")];
-		throw ex;
-	}
+	node.parentNode?.replaceChild(node.cloneNode(true), node);
 };
 
-for (const element of document.querySelectorAll("tr")) {
-	const onclick = element.getAttribute("onclick");
-
-	if (onclick?.includes("lookup")) {
-		element.removeAttribute("onclick");
-		unbind_event_listeners(element);
-	}
-
-	element.classList.add("sxplustr")
-
-	console.log(element.classList)
+for (const element of document.querySelectorAll("tr[onclick]")) {
+	element.removeAttribute("onclick");
+	unbind_event_listeners(element);
 }
