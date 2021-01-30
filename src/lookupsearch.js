e72e93724c5517ee64effb08e5df1905a3bdc6a7
Michael Hansen <raze0365@gmail.com>
2021-01-30 23:42:30 +0100

    Improve creation/injection of player lookup search field

--- src/lookupsearch.js
+++ src/lookupsearch.js
@@ -1,28 +1,68 @@
 if (user_is_staff) {
-	const maincont = document.querySelector(".main__cont");
+	const lookupField = document.querySelector("#lookupid");
 
-	if (maincont) {
-		let search = document.createElement("div");
-		search.className = "main-heading";
-		search.style = "display: block;";
-		search.innerHTML = `<div class="main-title">
-			<ol class="breadcrumb">
-				<li class="active">Spilleropslag</li>
-			</ol>
-		</div>
-		<div class="main-filter">
-			<form class="main-filter__search" target="_blank" method="get" action="lookup" onsubmit="this.submit(); this.reset(); return false;">
+	if (!lookupField) {
+		const mainHeading = document.querySelector(".main-heading");
+
+		if (mainHeading) {
+			const breadcrumb = mainHeading?.querySelector(".breadcrumb");
+
+			if (breadcrumb) {
+				const mainTitle = document.createElement("div");
+				mainTitle.className = "main-title";
+
+				breadcrumb.parentNode.insertBefore(mainTitle, breadcrumb);
+				mainTitle.appendChild(breadcrumb);
+			} else {
+				const mainTitle = document.createElement("div");
+				mainTitle.className = "main-title";
+				mainTitle.innerHTML = `<ol class="breadcrumb">
+					<li class="active">Spilleropslag</li>
+				</ol>`;
+
+				mainHeading.appendChild(mainTitle);
+			}
+
+			const search = document.createElement("div");
+			search.className = "main-filter";
+			search.innerHTML = `<form class="main-filter__search" target="_blank" method="get" action="lookup" onsubmit="this.submit(); this.reset(); return false;">
 				<div class="input-group">
-					<input type="text" id="lookupid" name="lookupid" placeholder="Søg på SteamID/Navn" class="form-control" autocomplete="off" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
+					<input type="text" id="lookupid" name="lookupid" placeholder="Søg på SteamID/Navn" class="form-control">
 					<span class="input-group-btn">
 						<button type="submit" class="btn btn-default">
 							<div class="fa fa-search"></div>
 						</button>
 					</span>
 				</div>
-			</form>
-		</div>`;
+			</form>`;
 
-		maincont.insertBefore(search, maincont.firstChild);
+			mainHeading.appendChild(search);
+		} else {
+			const mainCont = document.querySelector(".main__cont");
+
+			if (mainCont) {
+				const search = document.createElement("div");
+				search.className = "main-heading";
+				search.innerHTML = `<div class="main-title">
+					<ol class="breadcrumb">
+						<li class="active">Spilleropslag</li>
+					</ol>
+				</div>
+				<div class="main-filter">
+					<form class="main-filter__search" target="_blank" method="get" action="lookup" onsubmit="this.submit(); this.reset(); return false;">
+						<div class="input-group">
+							<input type="text" id="lookupid" name="lookupid" placeholder="Søg på SteamID/Navn" class="form-control">
+							<span class="input-group-btn">
+								<button type="submit" class="btn btn-default">
+									<div class="fa fa-search"></div>
+								</button>
+							</span>
+						</div>
+					</form>
+				</div>`;
+
+				mainCont.insertBefore(search, mainCont.firstChild);
+			}
+		}
 	}
 }
