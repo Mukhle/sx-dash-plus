a5224a72815bef4bcac91fcd1a707e803b0b1e12
Michael Hansen <raze0365@gmail.com>
2021-06-23 03:29:44 +0200

    Format release action javascript file

--- .github/actions/release/index.js
+++ .github/actions/release/index.js
@@ -1,79 +1,87 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const path = require('path');
-const fs = require('fs');
-const AdmZip = require('adm-zip');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const path = require("path");
+const fs = require("fs");
+const AdmZip = require("adm-zip");
 
 async function run() {
-	const token = core.getInput('token');
+	const token = core.getInput("token");
 	const octokit = github.getOctokit(token);
 
-	const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+	const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
 	core.info(`Repository ${owner}/${repo}`);
 
-	const currentVersion = await core.group('Get current version', async () => {
+	const currentVersion = await core.group("Get current version", async () => {
 		try {
-			const manifestPath = path.join(process.env.GITHUB_WORKSPACE, 'public/manifest.json')
-			const manifest = fs.readFileSync(manifestPath)
+			const manifestPath = path.join(process.env.GITHUB_WORKSPACE, "public/manifest.json");
+			const manifest = fs.readFileSync(manifestPath);
 			const { version } = JSON.parse(manifest);
 
 			core.info(`Detected version: ${version}`);
 
-			return version
+			return version;
 		} catch (error) {
 			core.setFailed(`Unable to get current version.`);
 			core.error(error);
 			process.exit();
 		}
-	})
+	});
 
-	const previousVersion = await core.group('Get release version', async () => {
+	const previousVersion = await core.group("Get release version", async () => {
 		try {
-			const { data: { tag_name: version } } = await octokit.repos.getLatestRelease({
+			const {
+				data: { tag_name: version },
+			} = await octokit.repos.getLatestRelease({
 				owner,
-				repo
+				repo,
 			});
 
 			core.info(`Previous release version: ${version}`);
 
 			if (version === currentVersion) {
-				core.info('Version has not changed.');
+				core.info("Version has not changed.");
 				process.exit();
 			}
 
-			return version
+			return version;
 		} catch (error) {
-			core.info('No previous release version.');
+			core.info("No previous release version.");
 		}
-	})
+	});
 
-	const changelog = previousVersion ? `https://github.com/${owner}/${repo}/compare/${previousVersion}...${currentVersion}` : `Initial release`
-	const upload_url = await core.group('Create new release', async () => {
+	const changelog = previousVersion
+		? `https://github.com/${owner}/${repo}/compare/${previousVersion}...${currentVersion}`
+		: `Initial release`;
+	const upload_url = await core.group("Create new release", async () => {
 		try {
-			const { data: { upload_url: url } } = await octokit.repos.createRelease({
+			const {
+				data: { upload_url: url },
+			} = await octokit.repos.createRelease({
 				owner,
 				repo,
 				tag_name: currentVersion,
 				name: currentVersion,
-				body: changelog
+				body: changelog,
 			});
 
 			core.info(`Created release for version ${currentVersion}`);
 
-			return url
+			return url;
 		} catch (error) {
-			core.setFailed(`Failed to create release ${currentVersion} for ${owner}/${repo}#${process.env.GITHUB_SHA}.`);
+			core.setFailed(
+				`Failed to create release ${currentVersion} for ${owner}/${repo}#${process.env.GITHUB_SHA}.`
+			);
 			core.error(error);
 			core.debug(JSON.stringify(error.headers));
 			core.debug(JSON.stringify(error.request));
 			process.exit();
 		}
-	})
+	});
 
-	const buffer = await core.group('Save zipped source to buffer', async () => {
+	const buffer = await core.group("Save zipped source to buffer", async () => {
 		try {
-			const sourcePath = path.join(process.env.GITHUB_WORKSPACE, 'dist')
+			const sourcePath = path.join(process.env.GITHUB_WORKSPACE, "dist");
 
 			const zip = new AdmZip();
 			zip.addLocalFolder(sourcePath);
@@ -81,24 +89,24 @@ async function run() {
 
 			core.info(`Stored zipped source to buffer.`);
 
-			return zipped
+			return zipped;
 		} catch (error) {
 			core.setFailed(`Failed to store zip.`);
 			core.error(error);
 			process.exit();
 		}
-	})
+	});
 
-	await core.group('Upload zipped buffer to release', async () => {
+	await core.group("Upload zipped buffer to release", async () => {
 		try {
 			await octokit.repos.uploadReleaseAsset({
 				url: upload_url,
 				headers: {
-					'content-type': 'application/zip',
-					'content-length': buffer.length
+					"content-type": "application/zip",
+					"content-length": buffer.length,
 				},
 				name: `SxDashPlus-${currentVersion}.zip`,
-				data: buffer
+				data: buffer,
 			});
 
 			core.info(`Uploaded asset to release.`);
@@ -107,7 +115,7 @@ async function run() {
 			core.error(error);
 			process.exit();
 		}
-	})
+	});
 }
 
-run()
+run();
