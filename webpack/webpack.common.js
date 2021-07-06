3fc34789a147de5974d9b7b579a2d4ffd6e2f79a
Michael Hansen <raze0365@gmail.com>
2021-07-07 00:25:33 +0200

    Double quotes to single quotes

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,46 +1,47 @@
 // const webpack = require('webpack');
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const srcDir = path.join(__dirname, "..", "src");
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const srcDir = path.join(__dirname, '..', 'src');
 
 module.exports = {
 	entry: {
-		"fixes/stafflist": path.join(srcDir, "fixes/stafflist.ts"),
-		"fixes/tables": path.join(srcDir, "fixes/tables.ts"),
-		"fixes/traficlaws": path.join(srcDir, "fixes/traficlaws.ts"),
-		"lookup/info": path.join(srcDir, "lookup/info.ts"),
-		"lookup/notes": path.join(srcDir, "lookup/notes.ts"),
-		"lookup/punishments": path.join(srcDir, "lookup/punishments.ts"),
-		"lookup/shared": path.join(srcDir, "lookup/shared.ts"),
-		"lookup/watcher": path.join(srcDir, "lookup/watcher.ts"),
-		lookupsearch: path.join(srcDir, "lookupsearch.ts"),
-		steamids: path.join(srcDir, "steamids.ts"),
+		'fixes/stafflist': path.join(srcDir, 'fixes/stafflist.ts'),
+		'fixes/tables': path.join(srcDir, 'fixes/tables.ts'),
+		'fixes/traficlaws': path.join(srcDir, 'fixes/traficlaws.ts'),
+		'lookup/info': path.join(srcDir, 'lookup/info.ts'),
+		'lookup/notes': path.join(srcDir, 'lookup/notes.ts'),
+		'lookup/punishments': path.join(srcDir, 'lookup/punishments.ts'),
+		'lookup/shared': path.join(srcDir, 'lookup/shared.ts'),
+		'lookup/watcher': path.join(srcDir, 'lookup/watcher.ts'),
+		lookupsearch: path.join(srcDir, 'lookupsearch.ts'),
+		steamids: path.join(srcDir, 'steamids.ts'),
 	},
 	output: {
-		path: path.join(__dirname, "../dist/js"),
-		filename: "[name].js",
+		path: path.join(__dirname, '../dist/js'),
+		filename: '[name].js',
 	},
 	optimization: {
 		splitChunks: {
-			name: "vendor",
-			chunks: "initial",
+			name: 'vendor',
+			chunks: 'initial',
 		},
+		minimize: false,
 	},
 	module: {
 		rules: [
 			{
 				test: /\.tsx?$/,
-				use: "ts-loader",
+				use: 'ts-loader',
 				exclude: /node_modules/,
 			},
 		],
 	},
 	resolve: {
-		extensions: [".ts", ".tsx", ".js"],
+		extensions: ['.ts', '.tsx', '.js'],
 	},
 	plugins: [
 		new CopyPlugin({
-			patterns: [{ from: ".", to: "../", context: "public" }],
+			patterns: [{ from: '.', to: '../', context: 'public' }],
 			options: {},
 		}),
 	],
