aa6cbd3e201b84274409c843337d0c2955f9592f
Michael Hansen <raze0365@gmail.com>
2022-06-08 01:16:59 +0200

    Only remove click listener for table rows which would redirect to lookup

--- src/fixes/tables.ts
+++ src/fixes/tables.ts
@@ -1,5 +1,8 @@
 export async function execute(): Promise<void> {
 	for (const element of document.querySelectorAll<HTMLTableRowElement>('.table > tbody > tr')) {
+		const attribute = element.getAttribute('onclick')
+		if (!attribute?.includes('lookup')) continue
+
 		element.removeAttribute('onclick')
 
 		const clone = element.cloneNode(true) as HTMLTableRowElement
